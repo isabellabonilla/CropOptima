2e84ab138e3f1a62445511f9dc7edf6c2397f0ea
isabellabonilla <isabellabonilla@ufl.edu>
2024-11-30 12:10:14 -0500

    Completed the bellman ford algorithm, which successfully creates the most optimal crop sequence provided the start and end crop to minimize nutrient waste. Also, made necessary modifications to the graph implementation to allow for the distinction between the differences in nutrients instead of grouping the differences (takes away from proper nutrient management in a real-life application).

--- CropOptimaProject/main.cpp
+++ CropOptimaProject/main.cpp
@@ -8,11 +8,17 @@ int main() {
     // get user input for specific state for crop rotation
     string state = "Alabama";
 
-    // loading crop data based on the specified state
-    vector<Crop> crops = readStateCropData("../CropOptimaProject/dataset/crop_nutrient_dataset.csv", state);
+    //loading crop data based on the specified state
+    vector<Crop> crops = readStateCropData("dataset/crop_nutrient_dataset.csv", state);
+
+    vector<Crop> newCrops;
+
+    for(int i = 0; i < 4; i++) {
+        newCrops.push_back(crops[i]);
+    }
 
     // create the graph based on how many crops are in the specified state
-    Graph cropGraph(crops);
+    Graph cropGraph(newCrops);
 
     // populate the graph with the proper compatibility values (edge weights)
     cropGraph.populate();
@@ -20,9 +26,13 @@ int main() {
     // print matrix for debugging
     cropGraph.printMatrix();
 
-    // get user input for the starting and ending crop for the rotation
+    //get user input for the starting and ending crop for the rotation
+    string startCrop = "Blue Java Banana";
+    string endCrop = "Longan";
 
-    // running the algorithms
+    //running the algorithms
+    cropGraph.bellmanFord(startCrop, endCrop);
+
     return 0;
 
 }
